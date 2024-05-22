697228fe7445fcef2ee417d547cd80d01af6c34b
Paulo Paim <paulo.paimsantos@gmail.com>
2024-05-22 17:38:43 -0300

    API enderecos utilizando a API ViaCep

---
+++ src/main/java/com/viacep/apiviacep/ApiviacepApplication.java
@@ -0,0 +1,13 @@
+package com.viacep.apiviacep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiviacepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiviacepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viacep/apiviacep/ApiviacepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viacep.apiviacep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiviacepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
